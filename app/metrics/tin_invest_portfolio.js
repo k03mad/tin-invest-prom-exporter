2928a5fcd2b9c3bd0640f633b493b02affdb8918
k03mad <k03.mad@gmail.com>
2023-08-03 18:11:05 +0300

    update name

--- app/metrics/tin_invest_portfolio.js
+++ app/metrics/tin_invest_portfolio.js
@@ -10,7 +10,7 @@ const figiToCurrency = {
 
 const nameReplacements = {
     '- привилегированные акции': 'прив.',
-    ' Вечный портфель ': 'ВП',
+    'Вечный портфель': 'ВП',
 };
 
 export default new client.Gauge({
